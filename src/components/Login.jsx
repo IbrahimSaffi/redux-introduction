345c3236309656192bef1c410127cc2dcb586356
IbrahimSaffi <ibrahimsaffinew@gmail.com>
2022-07-30 01:41:44 +0500

    commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import userSlice, {login,logout} from "../slices/userSlice"
+import { useDispatch } from 'react-redux'
+import {login,logout} from "../slices/userSlice"
 export default function Login() {
-  const user = useSelector(state=>state.userInfo)  
    let newloginDetails = {name :"ElliotAlderson",email:"elliotalderson@gmail.com",location:"USA"}
     const dispatch = useDispatch()
   return (
